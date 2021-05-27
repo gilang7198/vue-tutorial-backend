1c9f4a7b90fdc59385d59c62ebf8f4061cc25f0e
gilang7198 <33536996+gilang7198@users.noreply.github.com>
2021-05-27 09:22:45 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const messages = [
+    "halo",
+    "from",
+    "express"
+];
+
+app.get('/messages', (req, res) => {
+    res.send(messages);
+});
+
+app.post('/messages', (req, res) => {
+    let msg = req.body;
+    console.log(msg);
+    messages.push(msg.message);
+    res.json(msg)
+    console.log(messages);
+});
+
+app.listen(port, () => console.log('app running'));

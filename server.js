6f6bf8c1f09b8080bc9574074deed9abbed9114c
gilang7198 <gilang7198@gmail.com>
2021-05-27 16:27:54 +0700

    add register

--- server.js
+++ server.js
@@ -7,22 +7,46 @@ const port = 3000;
 app.use(cors());
 app.use(bodyParser.json());
 
-const messages = [
-    "halo",
-    "from",
-    "express"
+var messages = [
+    {
+        user: "Gilang",
+        text: "data awal"
+    },
+    {
+        user: "Gilang",
+        text: "data kedua"
+    },
+];
+var users = [
+    {
+        username: "Gilang",
+        password: "1"
+    }
 ];
 
 app.get('/messages', (req, res) => {
     res.send(messages);
 });
 
+app.get('/messages/:id', (req, res) => {
+    console.log(req.params.id);
+    res.send(messages[req.params.id]);
+});
+
 app.post('/messages', (req, res) => {
-    let msg = req.body;
-    console.log(msg);
-    messages.push(msg.message);
-    res.json(msg)
-    console.log(messages);
+    const userId = req.header('Authorization');
+    const user = users[userId];
+    let msg = {user: user.username, text: req.body.message};
+    messages.push(msg);
+    res.json(msg);
+});
+
+app.post('/register', (req, res) => {
+    let registerData = req.body;
+    let newIndex = users.push(registerData);
+    registerData.id = newIndex - 1;
+    console.log(registerData);
+    res.json(registerData);
 });
 
 app.listen(port, () => console.log('app running'));
